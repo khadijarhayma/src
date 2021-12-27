d8d9da6ae9b4355c95b76109c863c10cbfd2c074
khadijarhayma <94905625+khadijarhayma@users.noreply.github.com>
2021-12-27 23:13:47 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Button variant="primary">Primary</Button>{" "}
+        <Button variant="secondary">Secondary</Button>{" "}
+        <Button variant="success">Success</Button>{" "}
+        <Button variant="warning">Warning</Button>{" "}
+        <Button variant="danger">Danger</Button>{" "}
+        <Button variant="info">Info</Button>{" "}
+        <Button variant="light">Light</Button>{" "}
+        <Button variant="dark">Dark</Button>{" "}
+        <Button variant="link">Link</Button>
+      </>
+    </div>
+  );
+}
+
+export default App;
